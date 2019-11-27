5c0c81756eb3994d13c0fd51f1fdedd5c2aaad06
Keep <i@ickeep.com>
2019-11-27 11:52:27 +0800

    renderRoute

--- src/route/renderRoute.tsx
+++ src/route/renderRoute.tsx
@@ -1,7 +1,7 @@
-import React, {Component} from 'react'
-import {Route, Switch} from 'react-router-dom'
+import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
 import StoreRoute from './storeRoute'
-import P404 from "../page/p404";
+import P404 from '../page/p404'
 
 export interface IProps {
   rootPath: string,
@@ -9,35 +9,27 @@ export interface IProps {
   p404: any
 }
 
-// class _RouteComponent extends Component<any> {
-//   render() {
-//     const {children} = this.props;
-//     return children;
-//   }
-// }
-
-// const RouteComponent = withRouter(_RouteComponent);
 export default class RenderRoute extends Component<IProps> {
   router: any = null
 
 
   constructor(props: any) {
     super(props)
-    const {rootPath, routers, p404 = P404} = this.props
+    const { rootPath, routers, p404 = P404 } = this.props
     const arr: any[] = []
     routers.forEach((item: any) => {
       const path = rootPath + (item.path ? '/' + item.path : '');
       if (item.store && item.pages) {
-        arr.push({path, component: () => <><StoreRoute p404={p404} {...item} path={path}/></>})
+        arr.push({ path, component: () => <StoreRoute p404={p404} {...item} path={path} /> })
       } else {
-        arr.push({...item, path, component: item.component})
+        arr.push({ ...item, path })
       }
     })
-    arr.push({path: rootPath + '*', component: p404})
+    arr.push({ path: rootPath + '*', component: p404 })
     this.router = (
       <Switch>
         {arr.map(route => (
-          <Route key={route.path} {...route}/>
+          <Route key={route.path} {...route} />
         ))}
       </Switch>
     )
