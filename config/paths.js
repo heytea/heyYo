3126fd397087c02b8e8dd42a911d673c3ded81cf
Keep <i@ickeep.com>
2020-04-24 20:30:26 +0800

    优化 ts

--- config/paths.js
+++ config/paths.js
@@ -52,7 +52,7 @@ const resolveModule = (resolveFn, filePath) => {
 module.exports = {
   dotenv: resolveApp('.env'),
   appPath: resolveApp('.'),
-  appBuild: resolveApp('build'),
+  appBuild: resolveApp('dist'),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
   appIndexJs: resolveModule(resolveApp, 'src/index'),
@@ -66,7 +66,6 @@ module.exports = {
   appNodeModules: resolveApp('node_modules'),
   publicUrlOrPath,
 };
-
 
 
 module.exports.moduleFileExtensions = moduleFileExtensions;
