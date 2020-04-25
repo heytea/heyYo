3ee24a4d05a3590938e352f380f5d0e8141343c2
Keep <i@ickeep.com>
2020-04-25 10:56:40 +0800

    hook captcha

--- src/form/item/captcha.tsx
+++ src/form/item/captcha.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 import { observer } from 'mobx-react'
 import Input, { IProps as IInputProps } from './input'
 
@@ -14,60 +14,110 @@ interface IState {
   intervalID?: any
 }
 
-@observer
-export default class Captcha extends Component<IProps, IState> {
-  isUnmount: boolean
-
-  constructor(props: IProps) {
-    super(props)
-    this.state = { remain: 0, loading: false }
-    this.isUnmount = false
-  }
-
-  componentWillUnmount() {
-    const { intervalID } = this.state
-    intervalID && clearInterval(intervalID)
-    this.isUnmount = true
-  }
-
-  getCode = async () => {
-    const { remain, loading } = this.state
-    const { isActive, onGetCode } = this.props
+function Captcha(props: IProps) {
+  const [remain, setRemain] = useState(0)
+  const [loading, setLoading] = useState(false)
+  const [intervalID, setIntervalID] = useState(0)
+  const { isActive, className, onGetCode, ...args } = props
+  let isUnmount = false
+  useEffect(() => {
+    return () => {
+      isUnmount = true
+      intervalID && clearInterval(intervalID)
+    }
+  }, [])
+  const getCode = async () => {
     if (!loading && remain < 1 && isActive && typeof onGetCode === 'function') {
-      this.setState({ loading: true })
+      setLoading(true)
       const codeData = await onGetCode()
-      if (!this.isUnmount) {
-        this.setState({ loading: false })
+      if (isUnmount) {
+        setLoading(false)
         if (codeData.code === 0) {
           let remainNum = 60
           const tmpIntervalID = setInterval(() => {
             remainNum -= 1
-            this.setState({ remain: remainNum })
+            setRemain(remainNum)
             if (remainNum < 1) {
               clearInterval(tmpIntervalID)
             }
           }, 1000)
-          this.setState({ remain: remainNum, intervalID: tmpIntervalID })
+          setRemain(remainNum)
+          setIntervalID(intervalID)
         }
       }
     }
   }
-
-  render() {
-    const { isActive, className, onGetCode, ...args } = this.props
-    const { remain, loading } = this.state
-    return (
-      <Input
-        placeholder="验证码"
-        autoComplete="off"
-        {...args}
-        className={`c-captcha ${className || ''} `}
-        addonAfter={
-          <a href="javascript:;" className={`c-captcha-btn ${!loading && remain < 1 && isActive ? 'z-active' : ''}`}
-             onClick={this.getCode}>
-            {remain > 0 ? `${remain}S` : `获取验证码`}
-          </a>
-        }
-      />)
-  }
+  return (
+    <Input
+      placeholder="验证码"
+      autoComplete="off"
+      {...args}
+      className={`c-captcha ${className || ''} `}
+      addonAfter={
+        <a href="javascript:;" className={`c-captcha-btn ${!loading && remain < 1 && isActive ? 'z-active' : ''}`}
+           onClick={getCode}>
+          {remain > 0 ? `${remain}S` : `获取验证码`}
+        </a>
+      }
+    />)
 }
+
+export default observer(Captcha)
+
+// @observer
+// class Captcha1 extends Component<IProps, IState> {
+//   isUnmount: boolean
+//
+//   constructor(props: IProps) {
+//     super(props)
+//     this.state = { remain: 0, loading: false }
+//     this.isUnmount = false
+//   }
+//
+//   componentWillUnmount() {
+//     const { intervalID } = this.state
+//     intervalID && clearInterval(intervalID)
+//     this.isUnmount = true
+//   }
+//
+//   getCode = async () => {
+//     const { remain, loading } = this.state
+//     const { isActive, onGetCode } = this.props
+//     if (!loading && remain < 1 && isActive && typeof onGetCode === 'function') {
+//       this.setState({ loading: true })
+//       const codeData = await onGetCode()
+//       if (!this.isUnmount) {
+//         this.setState({ loading: false })
+//         if (codeData.code === 0) {
+//           let remainNum = 60
+//           const tmpIntervalID = setInterval(() => {
+//             remainNum -= 1
+//             this.setState({ remain: remainNum })
+//             if (remainNum < 1) {
+//               clearInterval(tmpIntervalID)
+//             }
+//           }, 1000)
+//           this.setState({ remain: remainNum, intervalID: tmpIntervalID })
+//         }
+//       }
+//     }
+//   }
+//
+//   render() {
+//     const { isActive, className, onGetCode, ...args } = this.props
+//     const { remain, loading } = this.state
+//     return (
+//       <Input
+//         placeholder="验证码"
+//         autoComplete="off"
+//         {...args}
+//         className={`c-captcha ${className || ''} `}
+//         addonAfter={
+//           <a href="javascript:;" className={`c-captcha-btn ${!loading && remain < 1 && isActive ? 'z-active' : ''}`}
+//              onClick={this.getCode}>
+//             {remain > 0 ? `${remain}S` : `获取验证码`}
+//           </a>
+//         }
+//       />)
+//   }
+// }
