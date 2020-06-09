b6c40254c3c8e85461f62d70672b1b72f2236f4c
Keep <i@ickeep.com>
2020-06-09 11:52:11 +0800

    多语言

--- src/lang/index.ts
+++ src/lang/index.ts
@@ -9,7 +9,22 @@ export const lang = {
   error_message: '错误信息',
   contact_admin: '请联系系统管理员',
   all: '全部',
-  out: '退出'
+  out: '退出',
+  validateMessages: {
+    equals: '两次输入不一致',
+    name: '用户名必须由6-16位数字/字母/中文组成',
+    phone: '手机必须由11位数字组成',
+    mail: '邮箱格式不正确',
+    phoneOrMail: '手机/邮箱格式不正确',
+    nameOrPhoneOrMail: '用户名/手机/邮箱格式不正确',
+    captcha: '验证码必须为6位数字',
+    imgCaptcha: '图形验证码必须为4位数字/字母',
+    password: '请输入8-16位的英文/数字',
+    url: '链接格式不正确',
+    chinese: '只允许中文',
+    number: '只允许数字',
+    amount: '金额格式不正确',
+  }
 }
 
 export default React.createContext(lang)
