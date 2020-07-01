8df2d8546dfeb894704279c8303998308a0b4a6a
Keep <i@ickeep.com>
2020-07-01 15:09:16 +0800

    template

--- src/page/_list/actionsRow.tsx
+++ src/page/_list/actionsRow.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { observer } from 'mobx-react-lite'
 import { Button, Modal } from 'antd'
 import { ButtonProps } from 'antd/lib/button'
-import template from "../../unit/template";
+import template, { templateToBoolean } from '../../unit/template'
 
 const confirm = Modal.confirm
 
@@ -17,21 +17,33 @@ export interface IProps {
 export interface IActionProps {
   store?: any,
   name: string,
+  whom: string,
   isConfirm: boolean,
   props?: ButtonProps
   record: any,
   val: any,
   index: number,
+  show?: string | boolean | Function
   url?: Function | string,
   action: Function
 }
 
 
-const Action = observer(({ store, name, props = {}, isConfirm, action, record, val, index, url = '' }: IActionProps) => {
+const Action = observer(({ store, name, props = {}, isConfirm, show = true, action, record, val, whom = '', index, url = '' }: IActionProps) => {
   const [newUrl, setNewUrl] = useState('')
+  const [isShow, setIsShow] = useState(true)
   useEffect(() => {
     setNewUrl(typeof url === 'function' ? url(val, record, index) : template(url, { ...record, _index: index }))
   }, [url])
+  useEffect(() => {
+    if (typeof show === 'boolean') {
+      setIsShow(show)
+    } else if (typeof show === 'function') {
+      setIsShow(show(record))
+    } else if (typeof show === 'string') {
+      setIsShow(templateToBoolean(show, { ...record, _index: index }))
+    }
+  }, [show, record])
   const execute = async () => {
     if (typeof action !== 'function') {
       console.error('action 必须是一个函数')
@@ -45,13 +57,18 @@ const Action = observer(({ store, name, props = {}, isConfirm, action, record, v
     if (isConfirm) {
       confirm({
         title: `${name}确认?`,
-        content: `您确定要批量${name}?`,
+        content: `您确定要 ${name} ${record[whom] || ''} ?`,
         onOk: () => execute()
       })
     } else {
       execute()
     }
   }
+
+  if (!isShow) {
+    return null
+  }
+
   const btnProps: ButtonProps = { size: 'small', htmlType: 'button' }
   if (newUrl) {
     btnProps.href = newUrl
--- src/unit/template.ts
+++ src/unit/template.ts
@@ -6,5 +6,28 @@ const template = (text = '', params: { [key: string]: string | number }) => {
     return (matchKey ? (typeof params[matchKey] === 'undefined' ? matchKey : params[matchKey]) : '') + ''
   })
 }
-
+export const templateToBoolean = (text = '', params: { [key: string]: string | number }): boolean => {
+  const str = template(text, params)
+  if (str.indexOf('===') > 0) {
+    const arr = str.split('===')
+    return arr[0] === arr[1]
+  }
+  if (str.indexOf('>') > 0) {
+    const arr = str.split('>')
+    return arr[0] > arr[1]
+  }
+  if (str.indexOf('>=') > 0) {
+    const arr = str.split('>=')
+    return arr[0] >= arr[1]
+  }
+  if (str.indexOf('<') > 0) {
+    const arr = str.split('<')
+    return arr[0] > arr[1]
+  }
+  if (str.indexOf('<=') > 0) {
+    const arr = str.split('<=')
+    return arr[0] >= arr[1]
+  }
+  return false
+}
 export default template
