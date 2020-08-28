79f958947546e20cdc199d128907b55771bb0ae9
Keep <i@ickeep.com>
2020-08-28 18:21:37 +0800

    远程加载组件

--- src/unit/remoteComponent.tsx
+++ src/unit/remoteComponent.tsx
@@ -6,17 +6,26 @@ const fnMap: { [key: string]: any } = {}
 
 export interface IProps {
   name: string,
+  url?: string,
   props?: { [key: string]: any },
   children?: any
 }
 
 export default function (props: IProps) {
-  const { name = '', props: CProps = {} } = props || {}
+  const { name = '', url = '', props: CProps = {} } = props || {}
   const { config: { remoteJsUrl = '', apiFormat: { code, msg, data }, codeSuccess }, Http: { httpGet } } = useContext(ConfigContext)
   const [RemoteC, setRemoteC] = useState(() => () => <Loading isCenter />)
-  const [isLoad, setIsLoad] = useState(false)
+  const [isLoad, setIsLoad] = useState(true)
   const [errMsg, setErrMsg] = useState('')
-  const fetchJs = async () => httpGet(remoteJsUrl, { name })
+  // const fetchJs = async () => httpGet(url ? url : remoteJsUrl + '/' + name + '.js')
+  const fetchJs = () => new Promise(function (resolve) {
+    const jsEl = document.createElement('script')
+    jsEl.setAttribute('type', 'text/javascript')
+    jsEl.setAttribute('src', url ? url : remoteJsUrl + '/' + name + '.js')
+    jsEl.onload = (e: any) => resolve(true)
+    jsEl.onerror = () => resolve(false)
+    document.body.appendChild(jsEl)
+  })
   const fetchDataSingle = () => {
     return fnMap[name] || (fnMap[name] = new Promise(function (resolve) {
       fetchJs().then((data) => {
@@ -25,43 +34,34 @@ export default function (props: IProps) {
       })
     }))
   }
-  // @ts-ignore
+  const runJs = () => {
+    try {
+      const C = window[name]
+      if (typeof C === 'function') {
+        setRemoteC(() => C)
+      } else if (C && typeof (C.default === 'function' || typeof C.default.type === 'function')) {
+        setRemoteC(() => C.default)
+      } else {
+        setErrMsg(`代码已加载，但 ${name} 类型为 ${typeof C}`)
+      }
+    } catch (e) {
+      setErrMsg(e.message || 'eval 出错了')
+    }
+  }
   const setC = async () => {
     setIsLoad(false)
-    const jsData = await fetchDataSingle()
-    if (jsData[code] !== codeSuccess) {
-      setErrMsg(jsData[msg] || '代码加载失败')
-    } else {
-      try {
-        // eslint-disable-next-line no-eval
-        window.eval(jsData[data])
-        // @ts-ignore
-        const C = window[name]
-        if (typeof C === 'function') {
-          setRemoteC(() => C)
-        } else {
-          setErrMsg(`代码已加载，但 ${name} 类型为 ${typeof C}`)
-        }
-      } catch (e) {
-        setErrMsg(e.message || 'eval 出错了')
-      }
-    }
+    await fetchDataSingle()
     setIsLoad(true)
+    runJs()
   }
   useEffect(() => {
-    // @ts-ignore
     const C = window[name]
-    if (typeof C === 'function') {
-      setRemoteC(() => C)
-      setIsLoad(true)
-    } else if (typeof C === 'undefined') {
+    if (typeof C === 'undefined') {
       setC()
     } else {
-      setIsLoad(true)
-      setErrMsg(`window 已存在 ${name} 但类型为 ${typeof C}`)
+      runJs()
     }
-    // eslint-disable-next-line
-  }, [name])
+  }, [name, setC])
   return (
     !isLoad ? <Loading isCenter /> : (errMsg ? <div>{errMsg}</div> : <RemoteC {...CProps} />)
   )
