c82460ff21df560f4723dc8e22393341b9e05c03
keepcai <keepcai@tencent.com>
2020-11-18 15:10:21 +0800

    HySelectRemote 空值不搜索

--- src/form/item/selectRemote.tsx
+++ src/form/item/selectRemote.tsx
@@ -17,7 +17,7 @@ export interface IProps extends ISelectProps {
 export default function HySelectRemote(props: IProps) {
   const [data, setData]: [Array<{ [key: string]: any }>, Function] = useState([])
   const [idMap, setIdMap]: [{ [key: string]: any }, Function] = useState({})
-  const [fetching, setFetching] = useState({})
+  const [fetching, setFetching] = useState(false)
   let lastFetchId = 0
   const { valKey = 'id', url, method = 'get', dataKey = 'data', value, valInKey = 'idIn', mode, apiKey, labelKey = 'name', onChange, onDataItem, nullData = [], ...args } = props
   const context = useContext(ConfigContext)
@@ -63,9 +63,13 @@ export default function HySelectRemote(props: IProps) {
   }
   const searchData = debounce(
     async (value: any) => {
-      const opt: { [key: string]: any } = {}
-      opt[apiKey || labelKey] = value
-      fetchData(opt)
+      if (value !== '') {
+        const opt: { [key: string]: any } = {}
+        opt[apiKey || labelKey] = value
+        fetchData(opt)
+      } else {
+        setData(nullData)
+      }
     }
     , 300)
 
@@ -98,109 +102,3 @@ export default function HySelectRemote(props: IProps) {
     </Select>
   );
 }
-
-// class SelectRemote extends Component<IProps> {
-//   static contextType = ConfigContext;
-//   static defaultProps = {
-//     value: '',
-//     method: 'get',
-//     labelKey: 'name',
-//     dataKey: 'data',
-//     valKey: 'id',
-//     valInKey: 'idIn'
-//   }
-//   lastFetchId: number
-//
-//   constructor(props: IProps) {
-//     super(props);
-//     this.lastFetchId = 0;
-//     this.searchData = debounce(this.searchData, 300);
-//   }
-//
-//   state = {
-//     data: [],
-//     idMap: {},
-//     fetching: false,
-//   };
-//   fetchData = async (opt: { [key: string]: any }) => {
-//     const { valKey = 'id', url, method = 'get', dataKey } = this.props
-//     if (url) {
-//       this.setState({ data: [], fetching: true })
-//       this.lastFetchId += 1
-//       const fetchId = this.lastFetchId
-//       const { Http, config } = this.context
-//       const { apiFormat, codeSuccess } = config
-//       const fn = method ? Http[`http${method}`] || Http.httpGet : Http.httpGet
-//       const dataData = await fn(url, opt)
-//       if (fetchId === this.lastFetchId && dataData[apiFormat.code] === codeSuccess) {
-//         const data = dataData[dataKey || apiFormat.data]
-//         const idMap: { [key: string]: any } = {}
-//         data.forEach && data.forEach((item: any) => {
-//           idMap[item[valKey]] = true
-//         })
-//         this.setState({ data, idMap, fetching: false });
-//       }
-//     }
-//   }
-//
-//
-//   initData = () => {
-//     const { valKey = 'id', value, valInKey = 'idIn', mode } = this.props
-//     if (typeof value !== 'undefined' && value !== '') {
-//       const opt: { [key: string]: any } = {}
-//       if (mode === 'multiple' || mode === 'tags') {
-//         opt[valInKey] = value
-//       } else {
-//         opt[valKey] = value
-//       }
-//       this.fetchData(opt)
-//     }
-//   }
-//
-//   searchData = async (value: any) => {
-//     const { apiKey, labelKey = 'name' } = this.props
-//     const opt: { [key: string]: any } = {}
-//     opt[apiKey || labelKey] = value
-//     this.fetchData(opt)
-//   }
-//
-//
-//   handleChange = (val: any) => {
-//     this.setState({ fetching: false, })
-//     const { onChange } = this.props
-//     if (onChange) {
-//       onChange(val)
-//     }
-//   };
-//
-//   componentDidMount() {
-//     this.initData()
-//   }
-//
-//   componentDidUpdate(prevProps: IProps) {
-//     const { value } = this.props
-//     const { data, idMap } = this.state;
-//     if (value !== prevProps.value) {
-//       // @ts-ignore
-//       if (value !== '' && ((!data || data.length < 1) || !idMap[value])) {
-//         this.initData()
-//       }
-//     }
-//   }
-//
-//   render() {
-//     const { fetching, data } = this.state;
-//     return (
-//       <Select
-//         {...this.props}
-//         data={data}
-//         notFoundContent={fetching ? <Spin size="small" /> : null}
-//         filterOption={false}
-//         showSearch={true}
-//         onSearch={this.searchData}
-//         onChange={this.handleChange}
-//       >
-//       </Select>
-//     );
-//   }
-// }
